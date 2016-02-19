9c80e02297d2f3d3875e9c99c56fbe44dd6c7d03
Lily Tsai <tslilyai@gmail.com>
2016-02-18 23:38:47 -0500

    add expected return values in protocols.py, implement wrappers for client-side stub calls

--- protocols.py
+++ protocols.py
@@ -13,22 +13,31 @@ class Protocol(object):
     def client_run(self, port):
 
     # MESSAGING 
+    # returns a success or error message string
     def send_message(self, src, dest, msg):
         self.P.send_message(src, dest, msg);
 
+    # returns a list of (from, msg) string tuples
     def get_messages(self, dest):
 
     # CREATION AND DELETION 
+    # returns a success or error message string
     def create_group(self, groupname):
+    # returns a success or error message string
     def create_account(self, username):
+    # returns a success or error message string
     def remove_account(self):
 
     # GROUPS 
+    # returns a success or error message string
     def edit_group_name(self, old_name, new_name):
+    # returns a success or error message string
     def remove_group_member(self, groupname, member):
+    # returns a success or error message string
     def add_group_member(self, groupname, member):
 
     # LISTING 
+    # returns a list of strings
     def list_groups(self, pattern):
     def list_accounts(self, pattern):
     def list_group_members(self, groupname):
