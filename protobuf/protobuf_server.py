b7ef25c3270b7a93d06d21f930510e858d46457d
Lily Tsai <tslilyai@gmail.com>
2016-02-19 20:31:05 -0500

    Add wrappers around database queries in protobuf_server.py

--- protobuf/protobuf_server.py
+++ protobuf/protobuf_server.py
@@ -10,28 +10,38 @@ class ProtobufServer(service_pb2.beta_create_ChatApp_server):
         self.db = DBManager()
 
     def rpc_send_individual_message(self, request, context):
-        to_id = self.db.get_user_id(request.to_name)
-        from_id = self.db.get_user_id(request.from_name)
-        msg = request.msg
-
         try:
+            to_id = self.db.get_user_id(request.to_name)
+            from_id = self.db.get_user_id(request.from_name)
+            msg = request.msg
             self.db.insert_message(to_id, from_id, msg)
         except Exception as e:
             return obj.Response(errno=1, msg=e)
         return obj.Response(errno=0, msg="success!\n")
     
     def rpc_send_group_message(self, request, context):
-        to_id = self.db.get_group_id(request.to_name)
-        from_id = self.db.get_user_id(request.from_name)
-        msg = request.msg
-
         try:
+            to_id = self.db.get_group_id(request.to_name)
+            from_id = self.db.get_user_id(request.from_name)
+            msg = request.msg
             self.db.insert_message(to_id, from_id, msg)
         except Exception as e:
             return obj.Response(errno=1, msg=e)
         return obj.Response(errno=0, msg="success!\n")
 
-    def rpc_get_messages(User) returns (stream CMessage) {}
+    def rpc_get_messages(self, request, context):
+        try:
+            u_id = self.db.get_user_id(request.username)
+            msgs = self.db.get_messages(u_id)
+        except Exception as e:
+            # return a dummy message if getting messages failed
+            yield obj.CMessage(
+                    to_name="NULL",
+                    from_name="NULL",
+                    msg="Could not retrieve messages\n"
+                )
+        for msg in self.db.get_messages(u_id):
+            yield msg
 
     def rpc_create_group(self, request, context):
         groupname = request.g_name
@@ -49,11 +59,54 @@ class ProtobufServer(service_pb2.beta_create_ChatApp_server):
             return obj.Response(errno=1, msg=e)
         return obj.Response(errno=0, msg="success!\n")
 
-    def rpc_remove_account(User) returns (Response) {}
-    def rpc_edit_group_name(Group) returns (Response) {}
-    def rpc_remove_group_member(Group) returns (Response) {}
-    def rpc_add_group_member(Group) returns (Response) {}
-    def rpc_list_group_members(Group) returns (stream User) {}
-    def rpc_list_groups(Pattern) returns (stream Group) {}
-    def rpc_list_accounts(Pattern) returns (stream User) {}
+    def rpc_remove_account(self, request, context):
+        try:
+            self.db.remove_account(request.username)
+        except Exception as e:
+            return obj.Response(errno=1, msg=e)
+        return obj.Response(errno=0, msg="success!\n")
 
+    def rpc_edit_group_name(self, request, context):
+        try:
+            self.db.edit_group_name(request.g_name, request.new_name)
+        except Exception as e:
+            return obj.Response(errno=1, msg=e)
+        return obj.Response(errno=0, msg="success!\n")
+
+    def rpc_remove_group_member(self, request, context):
+        try:
+            self.db.remove_group_member(request.g_name, request.edit_member_name)
+        except Exception as e:
+            return obj.Response(errno=1, msg=e)
+        return obj.Response(errno=0, msg="success!\n")
+
+    def rpc_add_group_member(self, request, context):
+        try:
+            self.db.remove_group_member(request.g_name, request.edit_member_name)
+        except Exception as e:
+            return obj.Response(errno=1, msg=e)
+        return obj.Response(errno=0, msg="success!\n")
+
+    def rpc_list_group_members(self, request, context):
+        try:
+            users = self.db.get_group_members(request.g_name)
+        except Exception as e:
+            return obj.User(username="NULL")
+        for user in users:
+            yield user
+
+    def rpc_list_groups(self, request, context):
+        try:
+            groups = self.db.get_groups(request.pattern)
+        except Exception as e:
+            return obj.Group(g_name="NULL")
+        for group in groups:
+            yield group
+        
+    def rpc_list_accounts(self, request, context):
+        try:
+            users = self.db.get_accounts(request.pattern)
+        except Exception as e:
+            return obj.User(username="NULL")
+        for user in users:
+            yield user 
